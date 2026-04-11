37a48aac53ae170f2c579259e1661b5a8b24b85f
movieplexbd <mia021697@gmail.com>
2026-04-11 22:55:24 +0600

    Add app/src/main/java/com/ottapp/moviestream/adapter/BannerAdapter.kt

---
+++ app/src/main/java/com/ottapp/moviestream/adapter/BannerAdapter.kt
@@ -0,0 +1,39 @@
+package com.ottapp.moviestream.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.ottapp.moviestream.data.model.Movie
+import com.ottapp.moviestream.databinding.ItemBannerBinding
+import com.ottapp.moviestream.util.loadImage
+
+class BannerAdapter(
+    private val onClick: (Movie) -> Unit
+) : ListAdapter<Movie, BannerAdapter.VH>(Diff()) {
+
+    inner class VH(private val b: ItemBannerBinding) : RecyclerView.ViewHolder(b.root) {
+        fun bind(movie: Movie) {
+            b.ivBanner.loadImage(movie.bannerImageUrl)
+            b.tvBannerTitle.text = movie.title
+            b.tvBannerCategory.text = movie.category
+            b.tvBannerRating.text = "⭐ ${movie.imdbRating}"
+            b.btnBannerPlay.setOnClickListener { onClick(movie) }
+            b.root.setOnClickListener { onClick(movie) }
+            if (movie.testMovie) b.tvFreeBadge.visibility = android.view.View.VISIBLE
+            else b.tvFreeBadge.visibility = android.view.View.GONE
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH =
+        VH(ItemBannerBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: VH, position: Int) =
+        holder.bind(getItem(position))
+
+    class Diff : DiffUtil.ItemCallback<Movie>() {
+        override fun areItemsTheSame(a: Movie, b: Movie) = a.id == b.id
+        override fun areContentsTheSame(a: Movie, b: Movie) = a == b
+    }
+}
