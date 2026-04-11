a53eabd7d9b73d09cc9961fcc3c0041eda71d97b
movieplexbd <mia021697@gmail.com>
2026-04-11 22:55:16 +0600

    Add app/src/main/java/com/ottapp/moviestream/ui/movies/MoviesFragment.kt

---
+++ app/src/main/java/com/ottapp/moviestream/ui/movies/MoviesFragment.kt
@@ -0,0 +1,76 @@
+package com.ottapp.moviestream.ui.movies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.ottapp.moviestream.R
+import com.ottapp.moviestream.adapter.MovieGridAdapter
+import com.ottapp.moviestream.data.model.Movie
+import com.ottapp.moviestream.databinding.FragmentMoviesBinding
+import com.ottapp.moviestream.util.Constants
+import com.ottapp.moviestream.util.hide
+import com.ottapp.moviestream.util.show
+
+class MoviesFragment : Fragment() {
+
+    private var _binding: FragmentMoviesBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: MoviesViewModel by viewModels()
+    private lateinit var adapter: MovieGridAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentMoviesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = MovieGridAdapter { movie -> openDetail(movie) }
+        binding.rvMovies.layoutManager = GridLayoutManager(requireContext(), 3)
+        binding.rvMovies.adapter = adapter
+
+        setupCategoryTabs()
+        observeViewModel()
+    }
+
+    private fun setupCategoryTabs() {
+        val tabs = listOf(
+            binding.tabAll      to Constants.CAT_ALL,
+            binding.tabBangla   to Constants.CAT_BANGLA,
+            binding.tabHindi    to Constants.CAT_HINDI,
+            binding.tabTrending to Constants.CAT_TRENDING
+        )
+        tabs.forEach { (btn, cat) ->
+            btn.setOnClickListener {
+                tabs.forEach { (b, _) -> b.isSelected = false }
+                btn.isSelected = true
+                viewModel.setCategory(cat)
+            }
+        }
+        binding.tabAll.isSelected = true
+    }
+
+    private fun observeViewModel() {
+        viewModel.loading.observe(viewLifecycleOwner) { loading ->
+            if (loading) binding.shimmer.show() else binding.shimmer.hide()
+        }
+        viewModel.filteredMovies.observe(viewLifecycleOwner) { movies ->
+            adapter.submitList(movies)
+            if (movies.isEmpty()) binding.tvEmpty.show() else binding.tvEmpty.hide()
+        }
+    }
+
+    private fun openDetail(movie: Movie) {
+        val bundle = bundleOf(Constants.EXTRA_MOVIE_ID to movie.id)
+        findNavController().navigate(R.id.action_movies_to_detail, bundle)
+    }
+
+    override fun onDestroyView() { _binding = null; super.onDestroyView() }
+}
